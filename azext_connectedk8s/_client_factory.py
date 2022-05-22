1b1e26d93b290282cdd82412c0287e8bec1cfaa7
Raki Rahman <mdrakiburrahman@gmail.com>
2022-05-22 11:53:04 -0400

    ConnectedK8s dump

---
+++ azext_connectedk8s/_client_factory.py
@@ -0,0 +1,60 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+from azure.cli.core.commands.client_factory import get_mgmt_service_client
+from azure.cli.core.profiles import ResourceType
+from azure.cli.core._profile import Profile
+from azure.cli.core.commands.client_factory import configure_common_settings
+from azure.cli.core.commands.client_factory import get_subscription_id
+from azure.graphrbac import GraphRbacManagementClient
+
+
+def cf_connectedk8s(cli_ctx, *_):
+    from azext_connectedk8s.vendored_sdks import ConnectedKubernetesClient
+    return get_mgmt_service_client(cli_ctx, ConnectedKubernetesClient)
+
+
+def cf_connected_cluster(cli_ctx, _):
+    return cf_connectedk8s(cli_ctx).connected_cluster
+
+
+def cf_connectedk8s_prev_2021_04_01(cli_ctx, *_):
+    from azext_connectedk8s.vendored_sdks.preview_2021_04_01 import ConnectedKubernetesClient
+    return get_mgmt_service_client(cli_ctx, ConnectedKubernetesClient)
+
+
+def cf_connected_cluster_prev_2021_04_01(cli_ctx, _):
+    return cf_connectedk8s_prev_2021_04_01(cli_ctx).connected_cluster
+
+
+def cf_resource_groups(cli_ctx, subscription_id=None):
+    return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES,
+                                   subscription_id=subscription_id).resource_groups
+
+
+def _resource_client_factory(cli_ctx, subscription_id=None):
+    return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES, subscription_id=subscription_id)
+
+
+def _resource_providers_client(cli_ctx):
+    from azure.mgmt.resource import ResourceManagementClient
+    return get_mgmt_service_client(cli_ctx, ResourceManagementClient).providers
+
+    # Alternate: This should also work
+    # subscription_id = get_subscription_id(cli_ctx)
+    # return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES, subscription_id=subscription_id).providers
+
+
+def _graph_client_factory(cli_ctx, **_):
+    profile = Profile(cli_ctx=cli_ctx)
+    cred, _, tenant_id = profile.get_login_credentials(
+        resource=cli_ctx.cloud.endpoints.active_directory_graph_resource_id)
+    client = GraphRbacManagementClient(cred, tenant_id,
+                                       base_url=cli_ctx.cloud.endpoints.active_directory_graph_resource_id)
+    configure_common_settings(cli_ctx, client)
+    return client
+
+
+def get_graph_client_service_principals(cli_ctx):
+    return _graph_client_factory(cli_ctx).service_principals
---
+++ azext_connectedk8s/commands.py
@@ -0,0 +1,36 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=line-too-long
+from azure.cli.core.commands import CliCommandType
+from azext_connectedk8s._client_factory import (cf_connectedk8s, cf_connected_cluster, cf_connectedk8s_prev_2021_04_01, cf_connected_cluster_prev_2021_04_01)
+from ._format import connectedk8s_show_table_format
+from ._format import connectedk8s_list_table_format
+
+
+def load_command_table(self, _):
+
+    connectedk8s_sdk = CliCommandType(
+        operations_tmpl='azext_connectedk8s.vendored_sdks.operations#ConnectedClusterOperations.{}',
+        client_factory=cf_connectedk8s
+    )
+    connectedk8s_sdk_prev = CliCommandType(
+        operations_tmpl='azext_connectedk8s.vendored_sdks.preview_2021_04_01.operations#ConnectedClusterOperations.{}',
+        client_factory=cf_connectedk8s_prev_2021_04_01
+    )
+    with self.command_group('connectedk8s', connectedk8s_sdk, client_factory=cf_connected_cluster) as g:
+        g.custom_command('connect', 'create_connectedk8s', supports_no_wait=True)
+        g.custom_command('update', 'update_agents')
+        g.custom_command('upgrade', 'upgrade_agents')
+        g.custom_command('delete', 'delete_connectedk8s', confirmation=True, supports_no_wait=True)
+        g.custom_command('enable-features', 'enable_features', is_preview=True)
+        g.custom_command('disable-features', 'disable_features', is_preview=True)
+        g.custom_command('list', 'list_connectedk8s', table_transformer=connectedk8s_list_table_format)
+        g.custom_show_command('show', 'get_connectedk8s', table_transformer=connectedk8s_show_table_format)
+        g.custom_command('proxy', 'client_side_proxy_wrapper')
+
+    with self.command_group('connectedk8s', connectedk8s_sdk_prev, client_factory=cf_connected_cluster_prev_2021_04_01) as g:
+        pass
+        # use this block for using preview sdk client for a command
